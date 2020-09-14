4a7d87461a7d418075b5a702e5e60ac3d578662c
Seema Anand Chaurasia <chourasia.seema@gmail.com>
2020-09-14 09:59:26 +0200

    added some method

--- src/test/java/booking/com/TC005_Booking_Verify_Crossbar_Products.java
+++ src/test/java/booking/com/TC005_Booking_Verify_Crossbar_Products.java
@@ -3,6 +3,7 @@ package booking.com;
 import com.relevantcodes.extentreports.LogStatus;
 import io.qameta.allure.*;
 import org.assertj.core.api.SoftAssertions;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import ksrtc.TestBase;
 import utils.Report;
@@ -21,6 +22,7 @@ public class TC005_Booking_Verify_Crossbar_Products extends TestBase {
 
     public void verifyCrossbarProducts() {
         EXTENT_REPORTS = Report.Instance(this.getClass().getName());
+
         try {
             CreateExtentReport(this.getClass().getName(), "Verify crossbar of Booking homepage");
             List<String> crossbarItems = getBookingHomepage().getCrossbarItems();
@@ -28,15 +30,24 @@ public class TC005_Booking_Verify_Crossbar_Products extends TestBase {
             EXTENT_TEST_LOGGER.log(LogStatus.INFO, "Booking homepage is loaded", EXTENT_TEST_LOGGER.addScreenCapture(Report.CaptureScreen(driver)));
             System.out.println("Crossbar items are:" + crossbarItems);
             EXTENT_TEST_LOGGER.log(LogStatus.INFO, "Crossbar products are:" + crossbarItems.toString());
-
-            //System.out.println("Number of Crossbar items:"+crossbarItems.size());
             List<String> expectedItems = Arrays.asList("Stays", "Flights", "Flight + Hotel", "Car rentals", "Attractions", "Airport taxis");
+
             //ContainsAll checks for all objects(Collection of objects) while contain checks for only one object
             softly.assertThat(Arrays.asList(crossbarItemsArray).containsAll(expectedItems)).isTrue();
         } catch (Exception exc) {
             PostConditionWithQuitDriver();
         } finally {
             softly.assertAll();
+        }
+    }
+    @AfterClass
+    void tearDown() {
+        try {
+            softly.assertAll();
+            PostCondition();
+        } catch (AssertionError Error) {
+            EXTENT_TEST_LOGGER.log(LogStatus.ERROR, Error.getLocalizedMessage(), EXTENT_TEST_LOGGER.addScreenCapture(Report.CaptureScreen(driver)));
+            PostConditionWithQuitDriver();
         }
     }
 }
