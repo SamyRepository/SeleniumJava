2ce2bc4ede668855c278bb9f0f4c8ff9e2376dba
Seema Anand Chaurasia <chourasia.seema@gmail.com>
2020-09-14 10:01:05 +0200

    added some method

--- src/test/java/booking/com/TC009_Booking_VerifyFooterTopMenu.java
+++ src/test/java/booking/com/TC009_Booking_VerifyFooterTopMenu.java
@@ -1,11 +1,13 @@
 package booking.com;
 
+import com.relevantcodes.extentreports.LogStatus;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Step;
 import io.qameta.allure.Story;
 import io.qameta.allure.Description;
 import org.assertj.core.api.SoftAssertions;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import ksrtc.TestBase;
 import utils.Report;
@@ -19,18 +21,35 @@ public class TC009_Booking_VerifyFooterTopMenu extends TestBase {
     @Test
     @Description("To get all links present at the Footer")
     @Story("Booking Homepage")
-    @Severity(SeverityLevel.CRITICAL)
+    @Severity(SeverityLevel.BLOCKER)
     @Step("To run TC009_Booking_VerifyFooterTopMenu.java")
 
-    public void toGetFooterNavigationLinksOnHomePage() {
+    public void toGetFooterTopMenu() {
         EXTENT_REPORTS = Report.Instance(this.getClass().getName());
-
-        SoftAssertions softly = new SoftAssertions();
-        List<String> footerNavigationLinks = getBookingHomepage().getFooterTopMenu();
-        String footerNavigationLinksArray[] = footerNavigationLinks.get(0).split("\\n?\\r");
-        System.out.println(Arrays.asList(footerNavigationLinksArray));
-        List<String> expectedFooterNavigationLinks = Arrays.asList("Mobile version", "Your account", "Make changes to your booking online",
-                "Customer Service help", "Become an affiliate", "Booking.com for Business");
-        softly.assertThat(expectedFooterNavigationLinks.containsAll(Arrays.asList(footerNavigationLinksArray)));
+        try {
+            CreateExtentReport(this.getClass().getName(), "Verify footer top menu items of Booking homepage");
+            SoftAssertions softly = new SoftAssertions();
+            List<String> footerTopMenu = getBookingHomepage().getFooterTopMenu();
+            String footerTopMenuArray[] = footerTopMenu.get(0).split("\\n?\\r");
+            System.out.println(Arrays.asList(footerTopMenuArray));
+            EXTENT_TEST_LOGGER.log(LogStatus.INFO, "Footer top menu items are: " +footerTopMenu.toString());
+            List<String> expectedFooterNavigationLinks = Arrays.asList("Mobile version", "Your account", "Make changes to your booking online",
+                    "Customer Service help", "Become an affiliate", "Booking.com for Business");
+            softly.assertThat(expectedFooterNavigationLinks.containsAll(Arrays.asList(footerTopMenuArray)));
+        } catch (Exception e) {
+            PostConditionWithQuitDriver();
+        } finally {
+            softly.assertAll();
+        }
+    }
+    @AfterClass
+    void tearDown() {
+        try {
+            softly.assertAll();
+            PostCondition();
+        } catch (AssertionError Error) {
+            EXTENT_TEST_LOGGER.log(LogStatus.ERROR, Error.getLocalizedMessage(), EXTENT_TEST_LOGGER.addScreenCapture(Report.CaptureScreen(driver)));
+            PostConditionWithQuitDriver();
+        }
     }
 }
