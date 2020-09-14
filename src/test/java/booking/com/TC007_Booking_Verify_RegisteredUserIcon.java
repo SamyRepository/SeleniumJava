c0cfafadaba18e29dcfdb55382d2366ec834b6be
Seema Anand Chaurasia <chourasia.seema@gmail.com>
2020-09-14 10:00:18 +0200

    added some method

--- src/test/java/booking/com/TC007_Booking_Verify_RegisteredUserIcon.java
+++ src/test/java/booking/com/TC007_Booking_Verify_RegisteredUserIcon.java
@@ -1,7 +1,9 @@
 package booking.com;
 
+import com.relevantcodes.extentreports.LogStatus;
 import io.qameta.allure.*;
 import org.assertj.core.api.SoftAssertions;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import ksrtc.TestBase;
 import utils.Report;
@@ -20,15 +22,34 @@ public class TC007_Booking_Verify_RegisteredUserIcon extends TestBase {
 
     public void verifyRegisteredUserIconOnHomepage() {
         EXTENT_REPORTS = Report.Instance(this.getClass().getName());
+        try {
+            CreateExtentReport(this.getClass().getName(), "Verify registered user icon items");
+            List<String> userIconItems = getBookingHomepage().clickOnSignIn().setUsername().setSignInPassword().selectUserIcon().getRegisteredUserProfileMenuList();
+            System.out.println("Registered User Icon items are:" + userIconItems.toString());
+            String userIconItemsArray[] = userIconItems.get(0).split("\\r?\\n");
+            System.out.println(Arrays.asList(userIconItemsArray));
+            EXTENT_TEST_LOGGER.log(LogStatus.INFO, "Registered user items are" + userIconItems.toString(), EXTENT_TEST_LOGGER.addScreenCapture(Report.CaptureScreen(driver)));
+            List<String> expectedRegisteredUserIconItems = Arrays.asList("Your account menu", "My dashboard", "Bookings", "Genius Loyalty Programme",
+                    "My reviews", "My wish lists", "Get the app", "Customer Service help", "Settings", "Travel Communities", "Exit menu");
+            softly.assertThat(expectedRegisteredUserIconItems.containsAll(Arrays.asList(userIconItemsArray)));
 
-        List<String> userIconItems = getBookingHomepage().clickOnSignIn().setUsername1()
-                                    .setSignInPassword1().selectUserIcon().getRegisteredUserProfileMenuList();
-        System.out.println("Registered User Icon items are:" + userIconItems.toString());
-        String userIconItemsArray[]=userIconItems.get(0).split("\\r?\\n");
-        System.out.println(Arrays.asList(userIconItemsArray));
-        List<String> expectedRegisteredUserIconItems=Arrays.asList("Your account menu", "My dashboard", "Bookings", "Genius Loyalty Programme",
-                "My reviews", "My wish lists", "Get the app", "Customer Service help", "Settings", "Travel Communities", "Exit menu");
-        softly.assertThat(expectedRegisteredUserIconItems.containsAll(Arrays.asList(userIconItemsArray)));
+        } catch (Exception e) {
+            PostConditionWithQuitDriver();
+        } finally {
+            softly.assertAll();
+        }
+    }
+
+    @AfterClass
+    void tearDown() {
+        try {
+            softly.assertAll();
+            PostCondition();
+        } catch (AssertionError Error) {
+            EXTENT_TEST_LOGGER.log(LogStatus.ERROR, Error.getLocalizedMessage(), EXTENT_TEST_LOGGER.addScreenCapture(Report.CaptureScreen(driver)));
+            PostConditionWithQuitDriver();
+        }
+
     }
 
 }
