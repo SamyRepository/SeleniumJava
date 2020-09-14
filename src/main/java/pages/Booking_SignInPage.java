19cb692ecfe68a9b873e67e6a2760490c67af818
Seema Anand Chaurasia <chourasia.seema@gmail.com>
2020-09-14 10:06:12 +0200

    added some method

--- src/main/java/pages/Booking_SignInPage.java
+++ src/main/java/pages/Booking_SignInPage.java
@@ -1,7 +1,9 @@
+
 package pages;
 
 import com.relevantcodes.extentreports.ExtentTest;
 import helper.SeleniumHelper;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.FindBy;
@@ -15,11 +17,14 @@ public class Booking_SignInPage extends PageBase {
     @FindBy(css = ".bui-button.bui-button--large.bui-button--wide")
     private WebElement NextButton;
 
-    @FindBy(xpath = "//span[@class='bui-button__text']")
+    @FindBy(xpath = "//span[@class='bui-button__text' and normalize-space(text()) = 'Sign in']")
     private WebElement SubmitSignInButton;
 
     @FindBy(css = "#password")
     private WebElement BookingSignInPassword;
+
+    @FindBy(xpath = "//span[contains(@class, 'bui-traveller-header__selection-text') and normalize-space(text()) = 'English']")
+    private WebElement SelectEnglishLanguage;
 
     Booking_SignInPage(RemoteWebDriver driver, ExtentTest EXTENT_TEST_LOGGER, SeleniumHelper helper) {
         super(driver, EXTENT_TEST_LOGGER, helper);
@@ -31,37 +36,43 @@ public class Booking_SignInPage extends PageBase {
         return false;
     }
 
-
-    public Booking_SignInPasswordPage setUsername1(){
+    public Booking_SignInPage setUsername() {
         Username.click();
         Username.clear();
-        Username.sendKeys(SignInUserNameLabels.USERNAME1.getLabel());
+        Username.sendKeys(SignInLoginCredentialLabels.USERNAME2.getLabel());
         NextButton.click();
-        return new Booking_SignInPasswordPage(driver,EXTENT_TEST_LOGGER,helper);
+        return new Booking_SignInPage(driver, EXTENT_TEST_LOGGER, helper);
     }
 
-    public Booking_SignInPasswordPage setUsername2(){
-        Username.click();
-        Username.clear();
-        Username.sendKeys(SignInUserNameLabels.USERNAME2.getLabel());
-        NextButton.click();
-        return new Booking_SignInPasswordPage(driver,EXTENT_TEST_LOGGER,helper);
+    public Booking_Homepage setSignInPassword() {
+        BookingSignInPassword.click();
+        BookingSignInPassword.clear();
+        BookingSignInPassword.sendKeys(SignInLoginCredentialLabels.PASSWORD2.getLabel());
+        SubmitSignInButton.click();
+        /*if (SelectEnglishLanguage.isDisplayed()) {
+            SelectEnglishLanguage.click();
+        }*/
+        return new Booking_Homepage(driver,EXTENT_TEST_LOGGER,helper);
     }
 
+    public Booking_Homepage submitLoginCredentials() {
+        SubmitSignInButton.click();
+        return new Booking_Homepage(driver, EXTENT_TEST_LOGGER, helper);
+    }
 
-    public enum SignInUserNameLabels{
-
+    public enum SignInLoginCredentialLabels {
         USERNAME1("chourasia.seema@gmail.com"),
-        USERNAME2("chaurasia.anand@gmail.com");
-        private final String label;
+        USERNAME2("chaurasia.anand@gmail.com"),
+        PASSWORD1("seemac123"),
+        PASSWORD2("Sharanya@323");
 
-        SignInUserNameLabels(String label) {
+        private final String label;
+        SignInLoginCredentialLabels(String label) {
             this.label = label;
         }
-
         public String getLabel() {
             return label;
         }
-        }
+    }
 
 }
