7e3f0dac0a5140a1052de5d69b60ef0466b5e2d8
Seema Anand Chaurasia <chourasia.seema@gmail.com>
2020-09-14 10:01:25 +0200

    added some method

--- src/test/java/booking/com/TC010_Booking_VerifyFooterNavigationLinks.java
+++ src/test/java/booking/com/TC010_Booking_VerifyFooterNavigationLinks.java
@@ -1,7 +1,9 @@
 package booking.com;
 
+import com.relevantcodes.extentreports.LogStatus;
 import io.qameta.allure.*;
 import org.assertj.core.api.SoftAssertions;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import ksrtc.TestBase;
 import utils.Report;
@@ -19,10 +21,26 @@ public class TC010_Booking_VerifyFooterNavigationLinks extends TestBase {
 
     public void getFooterNavigationLinks() {
         EXTENT_REPORTS = Report.Instance(this.getClass().getName());
+        try {
+            CreateExtentReport(this.getClass().getName(), "Verify footer top menu items of Booking homepage");
+            List<String> FooterNavigationLinks = getBookingHomepage().getFooterNavigationLinks();
+            System.out.println(FooterNavigationLinks);
+            EXTENT_TEST_LOGGER.log(LogStatus.INFO, "Footer navigation links are: " + FooterNavigationLinks.toString());
+        } catch (Exception exc) {
+            PostConditionWithQuitDriver();
+        } finally {
+            softly.assertAll();
+        }
+    }
 
-        List<String> FooterNavigationLinks = getBookingHomepage().getFooterNavigationLinks();
-        System.out.println(FooterNavigationLinks);
-
-
+    @AfterClass
+    void tearDown() {
+        try {
+            softly.assertAll();
+            PostCondition();
+        } catch (AssertionError Error) {
+            EXTENT_TEST_LOGGER.log(LogStatus.ERROR, Error.getLocalizedMessage(), EXTENT_TEST_LOGGER.addScreenCapture(Report.CaptureScreen(driver)));
+            PostConditionWithQuitDriver();
+        }
     }
 }
