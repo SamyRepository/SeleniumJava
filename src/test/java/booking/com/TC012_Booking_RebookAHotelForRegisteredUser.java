4b4fb4e13edf84a332bc3a322eee3428f9f0760e
Seema Anand Chaurasia <chourasia.seema@gmail.com>
2020-09-14 10:02:14 +0200

    added some method

--- src/test/java/booking/com/TC012_Booking_RebookAHotelForRegisteredUser.java
+++ src/test/java/booking/com/TC012_Booking_RebookAHotelForRegisteredUser.java
@@ -1,11 +1,12 @@
 package booking.com;
 
+import com.relevantcodes.extentreports.LogStatus;
 import io.qameta.allure.*;
 import org.assertj.core.api.SoftAssertions;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import ksrtc.TestBase;
 import utils.Report;
-
 
 
 public class TC012_Booking_RebookAHotelForRegisteredUser extends TestBase {
@@ -15,14 +16,31 @@ public class TC012_Booking_RebookAHotelForRegisteredUser extends TestBase {
     @Severity(SeverityLevel.CRITICAL)
     @Story("Booking Details of  registered user")
     @Description("To validate booking details of the registered user")
-    @Step("T run TC012_Booking_RebookHotelForRegisteredUser")
+    @Step("To run TC012_Booking_RebookHotelForRegisteredUser")
 
     public void rebookHotelForRegisteredUser() {
         EXTENT_REPORTS = Report.Instance(this.getClass().getName());
+        try {
+            CreateExtentReport(this.getClass().getName(), "Verify price of previously booked hotel");
+            String previousPrice = getBookingHomepage().clickOnSignIn().setUsername().setSignInPassword().selectUserIcon().
+                    clickOnBookingLinkOnRegisteredUserIcon().getHotelModernoPreviousPrice();
+            System.out.println("Hotel Moderno is previously booked at a price of :" + previousPrice);
+            EXTENT_TEST_LOGGER.log(LogStatus.INFO, "Hotel Moderno is previously booked at a price of :" +previousPrice);
 
-        String previousPrice = getBookingHomepage().clickOnSignIn().setUsername2().setSignInPassword2().selectUserIcon().
-                clickOnBookingLinkOnRegisteredUserIcon().getHotelModernoPreviousPrice();
-        System.out.println("Hotel Moderno is previously booked at a price of :" + previousPrice);
-
+        } catch (Exception e) {
+            PostConditionWithQuitDriver();
+        } finally {
+            softly.assertAll();
+        }
+    }
+    @AfterClass
+    void tearDown() {
+        try {
+            softly.assertAll();
+            PostCondition();
+        } catch (AssertionError Error) {
+            EXTENT_TEST_LOGGER.log(LogStatus.ERROR, Error.getLocalizedMessage(), EXTENT_TEST_LOGGER.addScreenCapture(Report.CaptureScreen(driver)));
+            PostConditionWithQuitDriver();
+        }
     }
 }
