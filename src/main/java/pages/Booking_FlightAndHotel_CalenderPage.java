3109dcf6a13dcca6d47b7cdba8167359a65dc8f7
Seema Anand Chaurasia <chourasia.seema@gmail.com>
2020-09-14 10:03:58 +0200

    added some method

--- src/main/java/pages/Booking_FlightAndHotel_CalenderPage.java
+++ src/main/java/pages/Booking_FlightAndHotel_CalenderPage.java
@@ -11,21 +11,14 @@ import org.openqa.selenium.support.PageFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.TimeUnit;
+
 public class Booking_FlightAndHotel_CalenderPage extends PageBase {
     private static final Logger LOGGER = LoggerFactory.getLogger(Booking_FlightAndHotel_CalenderPage.class);
-
-
-    /*@FindBy("");
-    private WebElement TodayDate;*/
-
-
-
-
 
     Booking_FlightAndHotel_CalenderPage(RemoteWebDriver driver, ExtentTest EXTENT_TEST_LOGGER, SeleniumHelper helper) {
         super(driver, EXTENT_TEST_LOGGER, helper);
         PageFactory.initElements(driver, this);
-
     }
 
     @Override
@@ -36,14 +29,17 @@ public class Booking_FlightAndHotel_CalenderPage extends PageBase {
             return false;
         }
     }
-
     private boolean waitUntilPageLoaded() {
         return driver.findElement(By.cssSelector(".floatingCalendar")).isDisplayed();
     }
 
     public Booking_FlightAndHotel_CalenderPage setDepartureDate(){
-        WebElement departureDate=driver.findElement(By.xpath("(//div[@class='calendarBoxText lmn-sw-responsive-form-field lmn-sw-tooltip-responsive__text'])[1]"));
-//        departureDate.click();
+        WebElement departureDate= driver.findElement(By.xpath("(//div[@class='calendarBoxText lmn-sw-responsive-form-field lmn-sw-tooltip-responsive__text'])[1]"));
+//        departureDate.sendKeys(Keys.TAB);
+//        WebDriverWait wait1 = new WebDriverWait(driver, 10);
+//        WebElement element1 = wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("(//div[@class='calendarBoxText lmn-sw-responsive-form-field lmn-sw-tooltip-responsive__text'])[1]")));
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        departureDate.click();
         departureDate.sendKeys(Keys.TAB);
         return this;
     }
@@ -51,6 +47,6 @@ public class Booking_FlightAndHotel_CalenderPage extends PageBase {
     public Booking_FlightAndHotel_NumberOfPassengers setReturnDate(){
      WebElement returnDate=driver.findElement(By.xpath("(//div[@class='calendarBoxText lmn-sw-responsive-form-field lmn-sw-tooltip-responsive__text'])[2]"));
      returnDate.sendKeys(Keys.TAB);
-        return new Booking_FlightAndHotel_NumberOfPassengers(driver,EXTENT_TEST_LOGGER,helper);
+     return new Booking_FlightAndHotel_NumberOfPassengers(driver,EXTENT_TEST_LOGGER,helper);
     }
 }
